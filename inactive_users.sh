b705c476cc0bf5d60ada074cd3d463984c388927
root <root@localhost.localdomain>
2018-10-21 08:47:35 -0400

    comment update in script

--- inactive_users.sh
+++ inactive_users.sh
@@ -9,6 +9,7 @@ ignore_wheel=$(/usr/sbin/groupmems -g wheel -l | /bin/sed -e "s/  /\\\|/g")
 for U in $(/bin/awk -F: '{ system("passwd -S " $1)}' /etc/passwd | /bin/grep " PS " | /bin/grep -v "$ignore_wheel" | /bin/grep -v 'root'| /bin/awk '{print $1}')
 do
 /usr/bin/lastlog -b 90 -u $U| /bin/awk '{print $1,": Last login:",$5,$6,$9 }' | /usr/bin/tail -n +2 >> $LOG
+# ensure that the log is readable
 /bin/chmod 644 $LOG
 done
 
